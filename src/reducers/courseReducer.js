5a5034842ad876c22e7c0d5f2b8e402be17c5132
olmelabs <32703675+olmelabs@users.noreply.github.com>
2018-05-09 17:39:49 +0100

    Module 11 fixed some issues.

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -13,7 +13,7 @@ export default function courseReducer(state=initialState.courses, action){
           ];
         case types.UPDATE_COURSE_SUCCESS:
           return [
-            ...state.filter(course => course.id != action.course.id),
+            ...state.filter(course => course.id !== action.course.id),
             Object.assign({}, action.course)
           ];
         default:
